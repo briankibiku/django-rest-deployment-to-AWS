9d3e90b46671a16d4397e016ba4e05a604b98870
Brian Kibiku <kibikukimani2@gmail.com>
2023-02-15 16:09:56 +0300

    added permissions to viewset

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -2,3 +2,9 @@ from rest_framework import permissions
 
 class UpdateOwnProfile(permissions.BasePermission):
     """Allow users to edit their own permissions"""
+
+    def has_object_permission(self, request, view, obj):
+        """check user is trying to edit their own profile"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.id == request.user.id
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 from rest_framework import status
+
+
 from profiles_api import serializers
 from profiles_api import models
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -88,3 +92,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle CRUD for user profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
